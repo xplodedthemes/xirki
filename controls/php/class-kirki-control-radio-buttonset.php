075bbe6df58ff8e0c1eac61d3f664a50665e312e
Georges Haddad <sales@xplodedthemes.com>
2020-08-14 20:45:29 -0400

    no message

--- controls/php/class-kirki-control-radio-buttonset.php
+++ controls/php/class-kirki-control-radio-buttonset.php
@@ -38,16 +38,16 @@ class Xirki_Control_Radio_Buttonset extends Xirki_Control_Base {
 	 * @access protected
 	 */
 	protected function content_template() {
-		?>
-		<# if ( data.label ) { #><span class="customize-control-title">{{{ data.label }}}</span><# } #>
-		<# if ( data.description ) { #><span class="description customize-control-description">{{{ data.description }}}</span><# } #>
-		<div id="input_{{ data.id }}" class="buttonset">
-			<# for ( key in data.choices ) { #>
-				<input {{{ data.inputAttrs }}} class="switch-input screen-reader-text" type="radio" value="{{ key }}" name="_customize-radio-{{{ data.id }}}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( key === data.value ) { #> checked="checked" <# } #>>
-					<label class="switch-label switch-label-<# if ( key === data.value ) { #>on <# } else { #>off<# } #>" for="{{ data.id }}{{ key }}">{{{ data.choices[ key ] }}}</label>
-				</input>
-			<# } #>
-		</div>
-		<?php
+		        ?>
+        <# if ( data.label ) { #><span class="customize-control-title">{{{ data.label }}}</span><# } #>
+        <# if ( data.description ) { #><span class="description customize-control-description">{{{ data.description }}}</span><# } #>
+        <div id="input_{{ data.id }}" class="buttonset">
+            <# for ( key in data.choices ) { #>
+            <input {{{ data.inputAttrs }}} class="switch-input screen-reader-text" type="radio" value="{{ key }}" name="_customize-radio-{{{ data.id }}}" id="{{ data.id }}{{ key }}" {{{ data.link }}}<# if ( key === data.value ) { #> checked="checked" <# } #> />
+            <label class="switch-label switch-label-<# if ( key === data.value ) { #>on <# } else { #>off<# } #>" for="{{ data.id }}{{ key }}">{{{ data.choices[ key ] }}}</label>
+            <# } #>
+        </div>
+        <?php
+
 	}
 }
